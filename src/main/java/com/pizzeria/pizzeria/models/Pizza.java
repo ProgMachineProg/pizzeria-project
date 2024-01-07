fe53a1ecffbad49d9eba0e3d5edeb61e69ab5d46
ProgMachineProg <progressmachineuseful18@gmail.com>
2024-01-07 19:15:14 +0200

    add models pizza

---
+++ src/main/java/com/pizzeria/pizzeria/models/Pizza.java
@@ -0,0 +1,23 @@
+package com.pizzeria.pizzeria.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name="pizzas")
+public class Pizza {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private double price;
+    private String photoUrl;
+}
