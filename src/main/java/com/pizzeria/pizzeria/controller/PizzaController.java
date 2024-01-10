1ddc3e2913ef7143d430f72aab6f0c144ac4b982
ProgMachineProg <progressmachineuseful18@gmail.com>
2024-01-10 21:19:29 +0200

    add pizzas-create html and update PizzaController get pizzas new

--- src/main/java/com/pizzeria/pizzeria/controller/PizzaController.java
+++ src/main/java/com/pizzeria/pizzeria/controller/PizzaController.java
@@ -2,6 +2,7 @@ package com.pizzeria.pizzeria.controller;
 
 
 import com.pizzeria.pizzeria.dto.PizzaDto;
+import com.pizzeria.pizzeria.models.Pizza;
 import com.pizzeria.pizzeria.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,10 +20,19 @@ public class PizzaController {
         this.pizzaService = pizzaService;
     }
 
-    @GetMapping
-    public String ListPizzas(Model model) {
+    @GetMapping("/pizzas")
+    public String listPizzas(Model model) {
         List<PizzaDto> pizzas = pizzaService.findAllPizza();
         model.addAttribute("pizzas", pizzas);
         return "pizzas-list";
     }
+
+    @GetMapping("/pizzas/new")
+    public String createClubForm(Model model) {
+        Pizza pizza = new Pizza();
+        model.addAttribute("pizza", pizza);
+        return "pizzas-create";
+    }
+
+
 }
