149a5a1552704748821cacc2dfc6fbf3b4351b03
ProgMachineProg <progressmachineuseful18@gmail.com>
2024-01-17 23:05:33 +0200

    add click button order

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -3,8 +3,6 @@ let closeCart = document.querySelector('.close');
 let body = document.querySelector('body');
 let listProductHTML = document.querySelector('.row.gx-4.gx-lg-5.row-cols-2.row-cols-md-3.row-cols-xl-4.justify-content-center');
 
-let addCart = document.querySelector('.addCart');
-
 let listProducts = [];
 iconCart.addEventListener('click', () => {
     body.classList.toggle('showCart')
@@ -22,41 +20,38 @@ const addDataToHTML = () => {
             newProduct.classList.add('col');
             newProduct.classList.add('mb-5')
             newProduct.innerHTML = `
-
-                
                     <div class="card h-100">
-
                         <img class="card-img-top" src="${product.photoUrl}" alt="..."/>
-
                         <div class="card-body p-4">
                             <div class="text-center">
-
                                 <h5 class="fw-bolder">${product.title}</h5>
-
                                 <span>${product.price}</span>
                                 <span>$</span>
                             </div>
                         </div>
-
                         <div class="buttons">
                             <div class="card-footer p-4 pt-0 border-top-0 bg-transparent d-flex justify-content-between">
                                 <div class="text-center"><a class="btn btn-outline-dark mt-auto"
                                                             href="/pizzas/${product.id}/edit">Edit</a>
                                 </div>
-                                <div class="addCart">
                                     <div class="text-center">
-                                        <button class="btn btn-outline-dark mt-auto" href="">Order</button>
-                                    </div>
+                                        <button class="addCart btn btn-outline-dark mt-auto" href="">Order</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                
             `;
             listProductHTML.appendChild(newProduct);
         })
     }
 }
+
+listProductHTML.addEventListener('click', (event) => {
+    let positionClick = event.target;
+    if(positionClick.classList.contains('addCart')) {
+        alert(1);
+    }
+})
 
 const initApp = () => {
     // get data from json
